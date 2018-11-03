37e2ef74ece07b668530e1ab5de31c08f960e411
pythonCat <pythoncat@qq.com>
2018-11-03 20:47:32 +0800

    基础share element 效果实现。
    发现一个bug： a->b,b->>a,a->b,b->>a 操作两次就会发现a上面除了shared elemtne view 之外，都看不到了[如果设置的转场动画不一致的时候会出现，比如一个是 Fade,一个是Explode]
    
    Signed-off-by: pythonCat <pythoncat@qq.com>

--- app/src/main/java/com/pycat/fuckshareelement/MainActivity.java
+++ app/src/main/java/com/pycat/fuckshareelement/MainActivity.java
@@ -2,10 +2,17 @@ package com.pycat.fuckshareelement;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.support.v4.view.ViewCompat;
 import android.transition.Explode;
+import android.view.View;
 
 public class MainActivity extends BaseActivity {
+
+    private View tvFirst;
+    private View tvSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,18 +20,34 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         getWindow().setEnterTransition(new Explode());
         getWindow().setExitTransition(new Explode());
+
+        tvFirst = findViewById(R.id.tv_first);
+        tvSecond = findViewById(R.id.tv_second);
+        ViewCompat.setTransitionName(tvFirst, "tv—first");
+        ViewCompat.setTransitionName(tvSecond, "tv—second");
         findViewById(R.id.img_first)
                 .setOnClickListener((view) -> {
                     startSecondActivity();
                 });
+
     }
 
     private void startSecondActivity() {
+        Pair[] pairs = {
+                new Pair<>(tvFirst, ViewCompat.getTransitionName(tvFirst)),
+                new Pair<>(tvSecond, ViewCompat.getTransitionName(tvSecond)),
+        };
         Intent intent = new Intent(get(), SecondActivity.class);
         @SuppressWarnings("unchecked")
         ActivityOptionsCompat optionsCompat = ActivityOptionsCompat
-                .makeSceneTransitionAnimation(this);
+                .makeSceneTransitionAnimation(this, pairs);
         Bundle bundle = optionsCompat.toBundle();
-        startActivity(intent, bundle);
+//        startActivity(intent, bundle);
+        ActivityCompat.startActivity(get(), intent, bundle);
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
     }
 }
--- app/src/main/java/com/pycat/fuckshareelement/SecondActivity.java
+++ app/src/main/java/com/pycat/fuckshareelement/SecondActivity.java
@@ -1,11 +1,14 @@
 package com.pycat.fuckshareelement;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
 import android.transition.Explode;
-import android.transition.Transition;
-import android.transition.TransitionInflater;
+import android.view.View;
 
 public class SecondActivity extends BaseActivity {
+
+    private View tvFirst;
+    private View tvSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,11 @@ public class SecondActivity extends BaseActivity {
         setContentView(R.layout.activity_second);
         getWindow().setEnterTransition(new Explode());
         getWindow().setExitTransition(new Explode());
+
+        tvFirst = findViewById(R.id.tv_first);
+        tvSecond = findViewById(R.id.tv_second);
+        ViewCompat.setTransitionName(tvFirst, "tv—first");
+        ViewCompat.setTransitionName(tvSecond, "tv—second");
     }
 
     @Override
