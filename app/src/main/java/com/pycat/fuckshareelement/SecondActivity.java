f64eabd02dff051fdf2713dc88814dea05825fff
pythonCat <pythoncat@qq.com>
2018-11-04 00:06:28 +0800

    glide异步加载时，让转场动画等图片加载完成再展示[完美~]

--- app/src/main/java/com/pycat/fuckshareelement/SecondActivity.java
+++ app/src/main/java/com/pycat/fuckshareelement/SecondActivity.java
@@ -1,10 +1,17 @@
 package com.pycat.fuckshareelement;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.view.ViewCompat;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.pycat.fuckshareelement.base.BaseActivity;
 import com.pycat.fuckshareelement.base.BaseKey;
 import com.pycat.fuckshareelement.base.GlideApp;
@@ -26,10 +33,27 @@ public class SecondActivity extends BaseActivity {
 
         ViewCompat.setTransitionName(img, imgUrl);
 
+        supportPostponeEnterTransition(); // 不要加载转场动画
         GlideApp.with(get()).load(imgUrl)
                 .centerCrop()
                 .placeholder(R.mipmap.ic_launcher)
                 .error(R.mipmap.ic_launcher_round)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model,
+                                                Target<Drawable> target, boolean isFirstResource) {
+
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model,
+                                                   Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        // 此时图片加载完成了
+                        supportStartPostponedEnterTransition(); // 开始加载转场动画
+                        return false;
+                    }
+                })
                 .into(img);
     }
 
