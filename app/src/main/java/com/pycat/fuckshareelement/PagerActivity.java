c26c1f01620ac5effc5ce783e4e71275698bedbd
pythonCat <pythoncat@qq.com>
2018-11-04 00:10:34 +0800

    recyclerView -> viewPager 加载转场动画显示成功[不滑动的情况下成功了]
    原因就是需要先等待，等图片准备好了之后，再去加载转场动画

--- app/src/main/java/com/pycat/fuckshareelement/PagerActivity.java
+++ app/src/main/java/com/pycat/fuckshareelement/PagerActivity.java
@@ -1,8 +1,10 @@
 package com.pycat.fuckshareelement;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
@@ -11,6 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.pycat.fuckshareelement.base.BaseActivity;
 import com.pycat.fuckshareelement.base.BaseKey;
 import com.pycat.fuckshareelement.base.GlideApp;
@@ -36,6 +42,7 @@ public class PagerActivity extends BaseActivity {
         mViewPager = findViewById(R.id.pager_view_pager);
         mViewPager.setAdapter(new Adapter(urlList));
         mViewPager.setCurrentItem(initPosition);
+        supportPostponeEnterTransition(); // 先不要加载转场动画
     }
 
     class Adapter extends PagerAdapter {
@@ -63,6 +70,22 @@ public class PagerActivity extends BaseActivity {
                     .error(R.mipmap.ic_launcher_round)
                     .placeholder(R.mipmap.ic_launcher)
                     .centerCrop()
+                    .listener(new RequestListener<Drawable>() {
+                        @Override
+                        public boolean onLoadFailed(@Nullable GlideException e, Object model,
+                                                    Target<Drawable> target, boolean isFirstResource) {
+                            supportStartPostponedEnterTransition(); // 开始加载转场动画
+                            return false;
+                        }
+
+                        @Override
+                        public boolean onResourceReady(Drawable resource, Object model,
+                                                       Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                            // 此时图片加载完成了
+                            supportStartPostponedEnterTransition(); // 开始加载转场动画
+                            return false;
+                        }
+                    })
                     .into(pagerImg);
             container.addView(view);
             return view;
